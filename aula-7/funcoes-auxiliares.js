06bad6a22fe3f993a4c044cf02ae7eb7aeb9ebf3
Isaque Rodrigues Rocha <isauqejk123@gmail.com>
2023-05-25 16:07:16 -0300

    atualização

--- aula-7/funcoes-auxiliares.js
+++ aula-7/funcoes-auxiliares.js
@@ -1,9 +1,17 @@
 //
 
+const entradas = [5,50,10,98,23];
+
+let i = 0;
+
+
 function gets(){
-    return 10;
+    const valor = entradas[i];
+    i++;
+    return valor;
 }
 
 function print(texto){
     console.log(texto);
 }
+module.exports = {gets, print};
